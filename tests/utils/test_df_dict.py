88fd317b1fe91653a4e9710af658f2390d37e24d
amin <amin.choudhury@profinda.com>
2025-02-04 20:59:26 +0000

    fix test

--- tests/utils/test_df_dict.py
+++ tests/utils/test_df_dict.py
@@ -4,4 +4,4 @@ from utils.df_dict import df_a
 def test_df_a():
     result = df_a()
     result_dict = result.to_dict(orient="records")
-    assert result_dict[0]["column1"] == 888
+    assert result_dict[0]["column1"] == 1
--- utils/df_dict.py
+++ utils/df_dict.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 a = [{"column1": 1, "column2": 2}, {"column1": 3, "column2": 4}]
 
+# lets see if my ec2 pulls the changes
+
 
 def df_a():
     return pd.DataFrame(a)
