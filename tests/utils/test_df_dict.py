1df5fd0c54e67c3938c821621b975d54f2a2fee5
Amin Choudhury <77303841+amzzz930@users.noreply.github.com>
2025-02-04 21:00:36 +0000

    lets-test-the-tests (#3)
    * update test
    
    * run tests when you push a PR
    
    * run tests when you push a PR
    
    * update test
    
    * update test
    
    * Update hooks
    
    * test pre-commit hooks
    
    * test pre-commit hooks
    
    * add pre-commit yaml
    
    * run black across all files
    
    * fix test
    
    ---------
    
    Co-authored-by: amin <amin.choudhury@profinda.com>

--- tests/utils/test_df_dict.py
+++ tests/utils/test_df_dict.py
@@ -1,11 +1,7 @@
-import sys
-from pathlib import Path
-
 from utils.df_dict import df_a
 
+
 def test_df_a():
-
     result = df_a()
-    result_dict = result.to_dict(orient='records')
-    assert result_dict[0]['column1'] == 1
-
+    result_dict = result.to_dict(orient="records")
+    assert result_dict[0]["column1"] == 1
--- utils/df_dict.py
+++ utils/df_dict.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-a = [{'column1': 1, 'column2': 2}, {'column1': 3, 'column2': 4}]
+a = [{"column1": 1, "column2": 2}, {"column1": 3, "column2": 4}]
+
+# lets see if my ec2 pulls the changes
+
 
 def df_a():
-
     return pd.DataFrame(a)
