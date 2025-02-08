b7bd3a84fba77ad0aea02ae5d94b0c6f88812f08
Amin Choudhury <77303841+amzzz930@users.noreply.github.com>
2025-02-08 16:43:11 +0000

    add-run (#10)
    * Update changes
    
    * push run
    
    * push run
    
    ---------
    
    Co-authored-by: amin <amin.choudhury@profinda.com>

---
+++ utils/weather_integration/run.py
@@ -0,0 +1,20 @@
+import logging
+from utils.weather_integration.transform import Transform
+
+CITIES = ["london", "paris", "berlin", "sylhet"]
+
+logger = logging.getLogger(__name__)
+
+
+class Run:
+    @classmethod
+    def run_integration(cls):
+        transform = Transform()
+
+        for city in CITIES:
+            try:
+                logger.info(f"Attempting to fetch and push data for {city}")
+                transform.push_to_postgres(city)
+                logger.info(f"Successfully pushed data for {city}\n")
+            except Exception as e:
+                logger.error(f"Failed to fetch or push data for {city}: {e}\n")
